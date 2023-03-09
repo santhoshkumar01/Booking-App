f4eef89777ff46feed0a3e622c4169f65a4962cf
santhoshKumar <neel.madhav@floworx.io>
2023-03-09 17:41:39 +0530

    Hotels Completed

--- Server/Controllers/HotelsController.js
+++ Server/Controllers/HotelsController.js
@@ -17,3 +17,75 @@ exports.createHotel = async (req, res) => {
         })
     }
 }
+
+
+// updateHotel
+exports.updateHotel = async (req, res) => {
+    try {
+        const updatedHotel = await Hotels.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+        res.status(200).json({
+            status: true,
+            data: updatedHotel,
+            message: 'Hotel Updated Successfully!'
+        })
+    } catch (error) {
+        console.log('Update Hotel Error' + error);
+        res.send(500).json({
+            status: false,
+            message: error
+        })
+    }
+}
+
+// deleteHotel
+exports.deleteHotel = async (req, res) => {
+    try {
+        await Hotels.findByIdAndDelete(req.params.id)
+        res.status(200).json({
+            status: true,
+            message: 'Hotel Deleted Successfully!'
+        })
+    } catch (error) {
+        console.log('Delete Hotel Error' + error);
+        res.send(500).json({
+            status: false,
+            message: error
+        })
+    }
+}
+
+//getSpecificHotel
+exports.getSpecificHotel = async (req, res) => {
+    try {
+        const getHotel = await Hotels.findById(req.params.id)
+        res.status(200).json({
+            status: true,
+            data: getHotel
+        })
+    } catch (error) {
+        console.log('getHotel Hotel Error' + error);
+        res.send(500).json({
+            status: false,
+            message: error
+        })
+    }
+}
+
+//getAllHotels
+exports.getAllHotels = async (req, res) => {
+    try {
+        const getHotels = await Hotels.find()
+        res.status(200).json({
+            status: true,
+            data: getHotels
+        })
+    } catch (error) {
+        console.log('getHotels Hotel Error' + error);
+        res.send(500).json({
+            status: false,
+            message: error
+        })
+    }
+}
+
+
--- Server/Routes/HotelsRoute.js
+++ Server/Routes/HotelsRoute.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
-const { createHotel } = require('../Controllers/HotelsController')
+const { createHotel, updateHotel, deleteHotel, getSpecificHotel, getAllHotels } = require('../Controllers/HotelsController')
 
 //create
 router.post('/', createHotel)
 //update
+router.put('/:id', updateHotel)
 //delete
+router.delete('/:id', deleteHotel)
 //get
+router.get('/:id', getSpecificHotel)
 //getall
+router.get('/', getAllHotels)
 
 module.exports = router
